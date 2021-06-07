ae1dc7ed169f5e2220b4758da6fac7fae3ffc1c7
Nidhi Parikh <nidhip@lanl.gov>
2021-06-07 13:05:03 -0600

    Added error checking for argument

--- makedir_if_not_exists.sh
+++ makedir_if_not_exists.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+############################################################################################
 # Makes directory if it does not exists.
+# Usage: ./makedir_if_not_exists.sh DIRECTORY_NAME
+# DIRECTORY_NAME: Name of the directory that needs to exist.
+############################################################################################
+
+if [ "$#" -lt 1 ] ; then
+    echo "Usage: ./makedir_if_not_exists.sh DIRECTORY_NAME"
+    echo "DIRECTORY_NAME: Name of the directory that needs to exist"
+    exit
+fi
 
 DIR_NAME=$1
 
