39519a586456c1ffbe8002dbe654ecfaecf46ff9
Nidhi Parikh <nidhip@lanl.gov>
2021-11-18 11:47:28 -0700

    fixed a typo

--- setup_turquoise_1.sh
+++ setup_turquoise_1.sh
@@ -1,23 +1,30 @@
 #!/bin/sh
 
 ###################################################################################################
-Currently, CIMMID code lives on gitlab.lanl.gov (yellow network) which is not accessible from turquoise network (e.g., Badger) where ELM is setup to run. So need to create bare DropZone repos on turquoise cluster. This script needs to be run once before running any experiemnts on turquoise network.
+# Currently, CIMMID code lives on gitlab.lanl.gov (yellow network) which is not accessible from turquoise network (e.g., Badger) where ELM is setup to run. So need to create bare DropZone repos on turquoise cluster. This script needs to be run once before running any experiemnts on turquoise network.
 
 # Usage: sh setup_turuoise_1.sh BARE_DROPZONE_REPO_PATH
-# BARE_DROPZONE_REPO_PATH: Path where bare DropZone repo is setup (e.g., /turquoise/users/nidhip/projects/cimmid)
+# BARE_DROPZONE_REPO_PATH: Path where bare DropZone repo is setup (e.g., /turquoise/users/nidhip/projects/cimmid/bare_dropzone_repos)
 
 # TO DO: Update above path to a path in project directory instead of in my home directory
 ###################################################################################################
 
 # Check for correct number of arguments.
-if [ "$#" -lt 1 ] || ! [ -d "$1" ] ; then
+if [ "$#" -lt 1 ] ; then
     echo -e "ERROR!! Incorrect number or type of arguments. See usage information below:\n"
     echo "Usage: sh setup_turuoise_1.sh BARE_DROPZONE_REPO_PATH"
     echo -e "ARE_DROPZONE_REPO_PATH: Path where bare DropZone repo is setup (e.g., /turquoise/users/nidhip/projects/cimmid)\n"
     exit
 fi
 
-BARE_DROPZONE_REPO_PATH=$1"
+BARE_DROPZONE_REPO_PATH=$1
+# If directory $BARE_DROPZONE_REPO_PATH does not exist, create it
+if [ ! -d "$BARE_DROPZONE_REPO_PATH" ]; then
+    mkdir $BARE_DROPZONE_REPO_PATH
+fi
+
+cd $BARE_DROPZONE_REPO_PATH
+BARE_DROPZONE_REPO_PATH=`pwd`
 
 # Create bare DropZone repo for integration model
 echo "$(date): creating bare DropZone repo for integration.."
