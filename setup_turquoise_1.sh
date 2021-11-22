fe98830ff3992911333213cfe7c74f325d61caea
Nidhi Parikh <nidhip@lanl.gov>
2021-11-22 10:02:33 -0700

    Updated setup_turquoise_1.sh and setup_yellow.sh to use a config file

--- setup_turquoise_1.sh
+++ setup_turquoise_1.sh
@@ -3,53 +3,74 @@
 ###################################################################################################
 # Currently, CIMMID code lives on gitlab.lanl.gov (yellow network) which is not accessible from turquoise network (e.g., Badger) where ELM is setup to run. So need to create bare DropZone repos on turquoise cluster. This script needs to be run once before running any experiemnts on turquoise network.
 
-# Usage: sh setup_turuoise_1.sh BARE_DROPZONE_REPO_PATH
-# BARE_DROPZONE_REPO_PATH: Path where bare DropZone repo is setup (e.g., /turquoise/users/nidhip/projects/cimmid/bare_dropzone_repos)
+# Usage: sh setup_turuoise_1.sh PATH_TO_MINICONDA_INSTALLATION CONFIG_FILE
+# PATH_TO_MINICONDA_INSTALLATION: Path where miniconda3 is installed (e.g., '/turquoise/users/nidhip/miniconda3')
+# CONFIG_FILE: Config file (e.g., cimmid.yaml)
 
 # TO DO: Update above path to a path in project directory instead of in my home directory
 ###################################################################################################
 
 # Check for correct number of arguments.
-if [ "$#" -lt 1 ] ; then
+if [ "$#" -lt 2 ] || ! [ -d "$1" ] || ! [ -f "$2" ]; then
     echo -e "ERROR!! Incorrect number or type of arguments. See usage information below:\n"
-    echo "Usage: sh setup_turuoise_1.sh BARE_DROPZONE_REPO_PATH"
-    echo -e "ARE_DROPZONE_REPO_PATH: Path where bare DropZone repo is setup (e.g., /turquoise/users/nidhip/projects/cimmid)\n"
+    echo "Usage: sh setup_turuoise_1.sh PATH_TO_MINICONDA_INSTALLATION CONFIG_FILE"
+    echo "PATH_TO_MINICONDA_INSTALLATION: Path where miniconda3 is installed (e.g., '/turquoise/users/nidhip/miniconda3')"
+    echo -e "CONFIG_FILE: Config file (e..g, cimmid.yaml)\n"
     exit
 fi
 
-BARE_DROPZONE_REPO_PATH=$1
+# ADD CIMMID miniconda path to PATH.
+MINICONDA_PATH=$1
+export PATH="$MINICONDA_PATH/bin:$PATH"
+conda config --prepend envs_dirs "$MINICONDA_PATH/envs"
+# Create virtual environment for integration
+conda create --name integration python=3.8
+conda activate integration
+conda install -c conda-forge shyaml
+
+# Get config file
+CONFIG_FILE=$2
+PROJECT_ROOT=`cat $CONFIG_FILE | shyaml get-value TURQUOISE_NET.PROJECT_ROOT`
+CONFIG_FILE="$PROJECT_ROOT/$2"
+
+# Create bare DropZone repo directory
+BARE_DROPZONE_REPO_PATH=`cat $CONFIG_FILE | shyaml get-value TURQUOISE_NET.BARE_DROPZONE_REPO_PATH`
 # If directory $BARE_DROPZONE_REPO_PATH does not exist, create it
 if [ ! -d "$BARE_DROPZONE_REPO_PATH" ]; then
     mkdir $BARE_DROPZONE_REPO_PATH
 fi
-
+# Store path for bare DropZone repo directory
 cd $BARE_DROPZONE_REPO_PATH
 BARE_DROPZONE_REPO_PATH=`pwd`
 
 # Create bare DropZone repo for integration model
+INTEGRATION_DIR=`cat $CONFIG_FILE | shyaml get-value INTEGRATION_REPO | rev | cut -d"/" -f1 | rev | cut -d"." -f1`
 echo "$(date): creating bare DropZone repo for integration.."
-INTEGRATION_BARE_DROPZONE_REPO_PATH="$BARE_DROPZONE_REPO_PATH/toy_model"
+INTEGRATION_BARE_DROPZONE_REPO_PATH="$BARE_DROPZONE_REPO_PATH/$INTEGRATION_DIR"
 mkdir $INTEGRATION_BARE_DROPZONE_REPO_PATH
 cd $INTEGRATION_BARE_DROPZONE_REPO_PATH
 git --bare init
 
 # Create bare DropZone repo for hydropop
+HYDROPO_DIR=`cat $CONFIG_FILE | shyaml get-value HYDROPOP_REPO | rev | cut -d"/" -f1 | rev | cut -d"." -f1`
 echo "$(date): creating bare DropZone repo for hydropop.."
-HYDROPOP_BARE_DROPZONE_REPO_PATH="$BARE_DROPZONE_REPO_PATH/hydropop"
+HYDROPOP_BARE_DROPZONE_REPO_PATH="$BARE_DROPZONE_REPO_PATH/$HYDROPO_DIR"
 mkdir $HYDROPOP_BARE_DROPZONE_REPO_PATH
 cd $HYDROPOP_BARE_DROPZONE_REPO_PATH
 git --bare init
 
 # Create bare DropZone repo for mosquito pop model
+MOSQUITO_POP_DIR=`cat $CONFIG_FILE | shyaml get-value MOSQUITO_POP_REPO | rev | cut -d"/" -f1 | rev | cut -d"." -f1`
 echo "$(date): creating bare DropZone repo for mosquito pop model.."
-MOSQUITO_POP_BARE_DROPZONE_REPO_PATH="$BARE_DROPZONE_REPO_PATH/mosquito-toy-model"
+MOSQUITO_POP_BARE_DROPZONE_REPO_PATH="$BARE_DROPZONE_REPO_PATH/$MOSQUITO_POP_DIR"
 mkdir $MOSQUITO_POP_BARE_DROPZONE_REPO_PATH
 cd $MOSQUITO_POP_BARE_DROPZONE_REPO_PATH
 git --bare init
 
 # Create bare DropZone repo for epi model
+EPI_DIR=`cat $CONFIG_FILE | shyaml get-value EPI_MODEL_REPO | rev | cut -d"/" -f1 | rev | cut -d"." -f1`
 echo "$(date): creating bare DropZone repo for epi model.."
-EPI_BARE_DROPZONE_REPO_PATH="$BARE_DROPZONE_REPO_PATH/human_epi_models"
+EPI_BARE_DROPZONE_REPO_PATH="$BARE_DROPZONE_REPO_PATH/$EPI_DIR"
 mkdir $EPI_BARE_DROPZONE_REPO_PATH
 cd $EPI_BARE_DROPZONE_REPO_PATH
 git --bare init
--- setup_yellow.sh
+++ setup_yellow.sh
@@ -3,72 +3,101 @@
 ###################################################################################################################################################################################
 # Currently, CIMMID code lives on gitlab.lanl.gov (yellow network) which is not accessible from turquoise network (e.g., Badger) where ELM is setup to run. So need to create working repos on a yellow network machine (e.g., your laptop or Darwin) from where code can be pulled on turquoise network. This script needs to be run once before running any experiemnts on yellow network.
 
-# Usage: sh setup_yellow.sh TURQUOISE_CLUSTER_LOGIN_NODE PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER
-# TURQUOISE_CLUSTER_LOGIN_NODE: Login node on turquoise network (e.g., ba-fe.lanl.gov)
-# PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER: Path where bare DropZone repositories are created on turquoise network (e.g., /turquoise/users/nidhip/projects/cimmid/integration/bare_dropzone_repos)
-
-# TO DO: Need to  update above path in my home directory to one under CIMMID project directory once it is created.
+# Usage: sh setup_yellow.sh PATH_TO_MINICONDA_INSTALLATION CONFIG_FILE
+# PATH_TO_MINICONDA_INSTALLATION: Path where miniconda3 is installed (e.g., '/projects/cimmid/miniconda3')
+# CONFIG_FILE: Config file (e.g., cimmid.yaml)
 ##################################################################################################################################################################################
 
-PROJECT_ROOT=`pwd`
-GITLAB_WORKING_REPO_NAME='gitlab_working_repo'
+# load/unload modules
+module unload gcc
+module load gcc/7.2.0
 
 # Check for correct number of arguments.
-if [ "$#" -lt 2 ]; then
+if [ "$#" -lt 2 ] || ! [ -d "$1" ] || ! [ -f "$2" ]; then
     echo -e "ERROR!! Incorrect number or type of arguments. See usage information below:\n"
-    echo "Usage: sh setup_yellow.sh TURQUOISE_CLUSTER_LOGIN_NODE PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER\n"
-    echo "TURQUOISE_CLUSTER_LOGIN_NODE: Login node on turquoise network (e.g., ba-fe.lanl.gov)\n"
-    echo -e "PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER: Path where bare DropZone repositories are created on turquoise network (e.g., /turquoise/users/nidhip/projects/cimmid/integration/bare_dropzone_repos)\n"
+    echo "Usage: sh setup_yellow.sh PATH_TO_MINICONDA_INSTALLATION CONFIG_FILE"
+    echo "PATH_TO_MINICONDA_INSTALLATION: Path where miniconda3 is installed (e.g., '/projects/cimmid/miniconda3')"
+    echo -e "CONFIG_FILE: Config file (e.g., cimmid.yaml)\n"
     exit
 fi
 
-TURQUOISE_CLUSTER_LOGIN_NODE=$1
-PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER=$2
+# ADD CIMMID miniconda path to PATH.
+MINICONDA_PATH=$1
+export PATH="$MINICONDA_PATH/bin:$PATH"
+conda config --prepend envs_dirs "$MINICONDA_PATH/envs"
+# Create virtual environment for integration
+conda create --name integration python=3.8
+conda activate integration
+conda install -c conda-forge shyaml
 
-mkdir $GITLAB_WORKING_REPO_NAME
-cd $GITLAB_WORKING_REPO_NAME
+# Get config file
+CONFIG_FILE=$2
+PROJECT_ROOT=`cat $CONFIG_FILE | shyaml get-value YELLOW_NET.PROJECT_ROOT`
+CONFIG_FILE="$PROJECT_ROOT/$2"
+
+# Get turquoise cluster login node and paths info
+TURQUOISE_CLUSTER_LOGIN_NODE=`cat $CONFIG_FILE | shyaml get-value TURQUOISE_NET.LOGIN_NODE`
+PROJECT_ROOT_ON_TURQUOISE_CLUSTER=`cat $CONFIG_FILE | shyaml get-value TURQUOISE_NET.PROJECT_ROOT`
+PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER=`cat $CONFIG_FILE | shyaml get-value TURQUOISE_NET.BARE_DROPZONE_REPO_PATH`
+PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER="$PROJECT_ROOT_ON_TURQUOISE_CLUSTER/$PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER"
+
+# Get path for gitlab working repo on yellow network
+GITLAB_WORKING_REPO_PATH=`cat $CONFIG_FILE | shyaml get-value YELLOW_NET.GITLAB_WORKING_REPO_PATH`
+GITLAB_WORKING_REPO_PATH="$PROJECT_ROOT/$GITLAB_WORKING_REPO_PATH"
+
+# Make gitlab working repo
+mkdir $GITLAB_WORKING_REPO_PATH
+cd $GITLAB_WORKING_REPO_PATH
 
 # Git clone integration model
 echo "$(date): cloning integration model.."
-git clone git@gitlab.lanl.gov:cimmid/integration/toy_model.git
-cd toy_model
+INTEGRATION_REPO=`cat $CONFIG_FILE | shyaml get-value INTEGRATION_REPO`
+git clone $INTEGRATION_REPO
+INTEGRATION_DIR=`echo $INTEGRATION_REPO | rev | cut -d"/" -f1 | rev | cut -d"." -f1`
+cd $INTEGRATION_DIR
 # Add mirror
-git remote add --mirror Turq-dev "$TURQUOISE_CLUSTER_LOGIN_NODE:$PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER/toy_model"
+git remote add --mirror Turq-dev "$TURQUOISE_CLUSTER_LOGIN_NODE:$PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER/$INTEGRATION_DIR"
 export GIT_SSH="$PROJECT_ROOT/turq-ssh-hop.sh"
 git push Turq-dev
-cd "$PROJECT_ROOT/$GITLAB_WORKING_REPO_NAME"
+cd $GITLAB_WORKING_REPO_PATH
 echo "$(date): done setting up working repo for integration.."
 echo ""
 
 # Git clone hydropop model
 echo "$(date): cloning hydropop model.."
-git clone git@gitlab.lanl.gov:cimmid/hydropop.git
-cd hydropop
+HYDROPOP_REPO=`cat $CONFIG_FILE | shyaml get-value HYDROPOP_REPO`
+git clone $HYDROPOP_REPO
+HYDROPO_DIR=`echo $HYDROPOP_REPO | rev | cut -d"/" -f1 | rev | cut -d"." -f1`
+cd $HYDROPO_DIR
 # Add mirror
-git remote add --mirror Turq-dev "$TURQUOISE_CLUSTER_LOGIN_NODE:$PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER/hydropop" 
+git remote add --mirror Turq-dev "$TURQUOISE_CLUSTER_LOGIN_NODE:$PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER/$HYDROPO_DIR" 
 git push Turq-dev
-cd "$PROJECT_ROOT/$GITLAB_WORKING_REPO_NAME"
+cd $GITLAB_WORKING_REPO_PATH
 echo "$(date): done setting up working repo for hydropop.."
 echo ""
 
 # Git clone mosquito pop model
 echo "$(date): cloning mosquito pop model.."
-git clone git@gitlab.lanl.gov:cimmid/earth_system_modeling/mosquito-toy-model.git
-cd mosquito-toy-model
+MOSQUITO_POP_REPO=`cat $CONFIG_FILE | shyaml get-value MOSQUITO_POP_REPO`
+git clone $MOSQUITO_POP_REPO
+MOSQUITO_POP_DIR=`echo $MOSQUITO_POP_REPO | rev | cut -d"/" -f1 | rev | cut -d"." -f1`
+cd $MOSQUITO_POP_DIR
 # Add mirror
-git remote add --mirror Turq-dev "$TURQUOISE_CLUSTER_LOGIN_NODE:$PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER/mosquito-toy-model"
+git remote add --mirror Turq-dev "$TURQUOISE_CLUSTER_LOGIN_NODE:$PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER/$MOSQUITO_POP_DIR"
 git push Turq-dev
-cd "$PROJECT_ROOT/$GITLAB_WORKING_REPO_NAME"
+cd $GITLAB_WORKING_REPO_PATH
 echo "$(date): done setting up working repo for mosquito pop.."
 echo ""
 
 # Git clone epi model
 echo "$(date): cloning epi model.."
-git clone git@gitlab.lanl.gov:cimmid/disease_and_human_modeling/human_epi_models.git
-cd human_epi_models
+EPI_MODEL_REPO=`cat $CONFIG_FILE | shyaml get-value EPI_MODEL_REPO`
+git clone $EPI_MODEL_REPO
+EPI_DIR=`echo $EPI_MODEL_REPO | rev | cut -d"/" -f1 | rev | cut -d"." -f1`
+cd $EPI_DIR
 # Add mirror
-git remote add --mirror Turq-dev "$TURQUOISE_CLUSTER_LOGIN_NODE:$PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER/human_epi_models"
+git remote add --mirror Turq-dev "$TURQUOISE_CLUSTER_LOGIN_NODE:$PATH_TO_BARE_DROPZONE_REPO_ON_TURQUOISE_CLUSTER/$EPI_DIR"
 git push Turq-dev
-cd "$PROJECT_ROOT/$GITLAB_WORKING_REPO_NAME"
+cd $GITLAB_WORKING_REPO_PATH
 echo "$(date): done setting up working repo for epi model .."
 echo ""
